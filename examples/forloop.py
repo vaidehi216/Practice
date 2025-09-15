4bd55ceb85e2e129ec177f1e623122f684faa1fd
vaidehi <vrudanii216@gmail.com>
2025-09-15 14:09:30 +0530

    [IMP]change a example

--- examples/forloop.py
+++ examples/forloop.py
@@ -29,11 +29,11 @@
 
 
 
-# q4
-# num = int(input("Enter a number: "))
 
-# for i in range(1, 11):
-#     print(f"{num} * {i} = {num * i}")
+num = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(f"{num} * {i} = {num * i}")
 
 
 
